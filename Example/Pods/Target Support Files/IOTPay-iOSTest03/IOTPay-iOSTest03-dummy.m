020ee72bb3036c0a8b044d69f56d441bde1eab06
Dan-YY <dan@dansstudio.ca>
2021-04-21 14:04:45 -0700

    init frist

---
+++ Example/Pods/Target Support Files/IOTPay-iOSTest03/IOTPay-iOSTest03-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_IOTPay_iOSTest03 : NSObject
+@end
+@implementation PodsDummy_IOTPay_iOSTest03
+@end
---
+++ Example/Pods/Target Support Files/Pods-IOTPay-iOSTest03_Example/Pods-IOTPay-iOSTest03_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_IOTPay_iOSTest03_Example : NSObject
+@end
+@implementation PodsDummy_Pods_IOTPay_iOSTest03_Example
+@end
